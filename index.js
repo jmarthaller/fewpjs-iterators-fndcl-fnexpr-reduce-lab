59bce1e68038169a4e277995d8ebe5e31bc1238d
Jacob Marthaller <https://github.com/jmarthaller>
2021-01-13 09:01:46 -0500

    commit tree f92296f39d9b05d9f6238a98fa09b470de55c654

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+const batteryBatches = [4, 5, 3, 4, 4, 6, 5]; 
+let totalBatteries = batteryBatches.reduce((total, batches) => total + batches)
 
 // Code your solution here
+// function batteries() {
+//     let totalBatteries = batteryBatches.reduce((a, b) => a+b)
+//     return totalBatteries
+// }
